dd70aa9d9aeeec84295223abb1487dc424dcdc68
Marcus Pereira <mpereirassa@gmail.com>
2021-02-13 11:34:41 -0300

    create config for development mode

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,15 @@
+require 'sinatra'
 require 'byebug'
 require 'mongoid'
 require 'require_all'
 
-require_all 'app'
 
 configure :testing do
   Mongoid.load!('config/mongoid.yml', :testing)
 end
+
+configure :development do
+  Mongoid.load!('config/mongoid.yml', :development)
+end
+
+require_all 'app'
