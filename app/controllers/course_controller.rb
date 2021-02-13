9c9ccd05e51c1b04ee4383bbd3ca1239ecb27289
Marcus Pereira <mpereirassa@gmail.com>
2021-02-13 11:44:08 -0300

    fix layout

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -1,6 +1,6 @@
 class CourseController < ApplicationController
   get '/curso/:uuid' do |uuid|
     @course = Course.find_by(uuid: uuid)
-    erb :single
+    erb :single, :layout => :base
   end
 end
