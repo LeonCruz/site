e926db0c1f7c1dd23689a491204cb609876b77fa
Marcus Pereira <mpereirassa@gmail.com>
2021-02-13 02:31:52 -0300

    require all

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,9 @@
-require 'require_all'
+require 'byebug'
 require 'mongoid'
+require 'require_all'
 
+require_all 'app'
 
 configure :testing do
   Mongoid.load!('config/mongoid.yml', :testing)
 end
-
-
-require_all 'app'
