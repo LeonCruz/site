315d6f817374b0b6d2957d29c186ee8013faaeb9
Leon Cruz <leon.cruz.teixeira@gmail.com>
2021-02-14 12:57:38 -0300

    enable sessions for store values

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ require 'sinatra/contrib'
 class ApplicationController < Sinatra::Base
   set :views, 'app/views'
   set :public_dir, 'app/public'
+  enable :sessions
 
   configure :development do
     register Sinatra::Reloader
